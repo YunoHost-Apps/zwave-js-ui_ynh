1bf74ca41cbcf6007fb1b6af50d38c9a014cf368
Krakinou <nicolas@laudrin.com>
2026-02-04 14:26:06 +0100

    Remove deprecated executable

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-#if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
-#then
-#    file_exec="zwave-js-ui"
-#else
-#    file_exec="zwave-js-ui-linux"
-#fi
-
 MEMORY_NEEDED="2048"
