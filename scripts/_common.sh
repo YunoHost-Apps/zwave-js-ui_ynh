46437983768cd544d3c405a672b78539fd1aa76d
Krakinou <nicolas@laudrin.com>
2026-02-04 14:07:59 +0100

    Add swap during install if required

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,11 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
-then
-    file_exec="zwave-js-ui"
-else
-    file_exec="zwave-js-ui-linux"
-fi
+#if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
+#then
+#    file_exec="zwave-js-ui"
+#else
+#    file_exec="zwave-js-ui-linux"
+#fi
+
+MEMORY_NEEDED="2048"
